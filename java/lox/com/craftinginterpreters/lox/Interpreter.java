34ac2f7adbcdc830e8641438a892f4ddc4fd6b52
Sagar Negi <sxxrfx@gmail.com>
2022-11-16 12:38:59 +0530

    Chapter 7: Final Done.

---
+++ java/lox/com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,136 @@
+package com.craftinginterpreters.lox;
+
+
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+    
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                checkNumberOperand(expr.operator, right);
+                return !isTruthy(right);
+            case MINUS:
+                return -(double) right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object) {
+        if (object == null)
+            return "nil";
+        
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+    
+    private void checkNumberOperand(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be numbers.");
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null)
+            return false;
+        
+        if (object instanceof Boolean)
+            return (boolean) object;
+        
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null)
+            return true;
+        if (a == null)
+            return false;
+
+        return a.equals(b);
+
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+    
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                checkNumberOperand(expr.operator, left, right);
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                checkNumberOperand(expr.operator, left, right);
+                return (double) left >= (double) right;
+            case LESS:
+                checkNumberOperand(expr.operator, left, right);
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                checkNumberOperand(expr.operator, left, right);
+                return (double) left <= (double) right;
+            case MINUS:
+                checkNumberOperand(expr.operator, left, right);
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+            case SLASH:
+                checkNumberOperand(expr.operator, left, right);
+                return (double) left / (double) right;
+            case STAR:
+                checkNumberOperand(expr.operator, left, right);
+                return (double) left * (double) right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+        }
+
+        // Unreachable.
+        return null;
+    }
+}
---
+++ java/lox/com/craftinginterpreters/lox/RuntimeError.java
@@ -0,0 +1,11 @@
+package com.craftinginterpreters.lox;
+
+class RuntimeError extends RuntimeException{
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+    
+}
