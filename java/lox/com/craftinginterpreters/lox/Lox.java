20d0b03496c9af78beae90aedb6c454a00cb2ac4
Sagar Negi <sxxrfx@gmail.com>
2022-11-16 11:35:44 +0530

    Chapter 6: Done!!!

--- java/lox/com/craftinginterpreters/lox/Lox.java
+++ java/lox/com/craftinginterpreters/lox/Lox.java
@@ -49,10 +49,13 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -62,5 +65,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.out.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at the end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
--- java/lox/com/craftinginterpreters/tool/GenerateAst.java
+++ java/lox/com/craftinginterpreters/tool/GenerateAst.java
@@ -35,14 +35,36 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // The AST classes.
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        // The base accept() method.
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println(
+                            "        R visit"
+                            + typeName + baseName + "("
+                            + typeName + " "
+                            + baseName.toLowerCase() + ");"
+            );
+        }
+        writer.println("    }");
+        writer.println();
     }
 
     private static void defineType(
@@ -59,11 +81,18 @@ public class GenerateAst {
         // Store parameters in fields.
         String[] fields = fieldList.split(", ");
         for (String field : fields) {
-            String name = field.split(" ")[0];
+            String name = field.split(" ")[1];
             writer.println("            this." + name + " = " + name + ";");
         }
 
-        writer.println("    }");
+        writer.println("        }");
+
+        // Visitor pattern.
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
 
         // Fields.
         writer.println();
@@ -71,6 +100,7 @@ public class GenerateAst {
             writer.println("       final " + field + ";");
         }
         writer.println("    }");
+        writer.println();
     }
     
 }
